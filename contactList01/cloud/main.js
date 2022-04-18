48ff9a3413117581c46b981c029650a955e9b829
catalunha <catalunha.mj@gmail.com>
2022-04-18 10:58:17 -0300

    estudos

--- contactList01/cloud/main.js
+++ contactList01/cloud/main.js
@@ -1,21 +1,3 @@
-const Contact = Parse.Object.extend('Contact');
-
-// The example below shows you how a cloud code function looks like.
-// Parse Server 3.x
-Parse.Cloud.define("hello", (request) => {
-	return("contactList01 - backend ok...");
-});
-
-Parse.Cloud.define('create-contact',async (req)=>{
-	const contact = new Contact();
-	contact.set('name',req.params.name);
-	contact.set('man',false);
-	contact.set('birthday',new Date(1972,09,11));
-	const savedContact = await contact.save();
-	return savedContact;
-});
-
-// If you have set a function in another cloud code file, called "test.js" (for example)
-// you need to refer it in your main.js, as you can see below:
-
-/* require("./test.js"); */
+require("./contact.js");
+require('./address.js');
+require('./user.js');
---
+++ contactList01/cloud/user.js
@@ -0,0 +1,41 @@
+Parse.Cloud.define('user-signup',async (req)=>{
+  const {name,email,password} = req.params;
+  if(name===null||name==='') throw 'user-register: Falta name';
+  if(email===null||email==='') throw 'user-register: Falta email';
+  if(password===null||password==='') throw 'user-register: Falta password';
+  const user = new Parse.User();
+  user.set('name',name);
+  user.set('username',email);
+  user.set('email',email);
+  user.set('password',password);
+  // const userNew = await user.signUp();
+  const userNew = await user.save(null,{useMasterKey:true});
+  return userNew;
+});
+
+Parse.Cloud.define('user-current', async (req)=>{
+  console.log('req.user: ',req.user);
+  console.log('Parse.User.current(): ',Parse.User.current());
+  console.log('Parse.User.authenticated(): ',Parse.User.authenticated);
+  return {
+    requser:req.user, 
+    usercur:Parse.User.current(),
+    userauth:Parse.User.authenticated
+  };
+  // let currentUser = req.user;
+  // currentUser.setACL(new Parse.ACL(currentUser));
+  // return await currentUser.save(null, { useMasterKey: true });
+});
+
+Parse.Cloud.define('user-login', async (req)=>{
+  const {email,password} = req.params;
+  if(email===null||email==='') throw "user-login: Falta email";
+  if(password===null||password==='') throw "user-login: Falta password";
+  const user = await Parse.User.logIn(email,password);
+  return user;
+});
+
+Parse.Cloud.define('user-logout',async (req)=>{
+  const user = await Parse.User.logOut();
+  return {user:user,msg:'user-logout: bem sucedido'};
+});
